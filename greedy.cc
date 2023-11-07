fff122b76fe1a0129c87f210c268b3f7cd11dbb7
CristinaTeixido <133266907+CristinaTeixido@users.noreply.github.com>
2023-11-07 22:15:18 +0100

    Create greedy.cc

---
+++ greedy.cc
@@ -0,0 +1,4 @@
+"Ha d’implementar un algorisme golafre. Degut a la dificultat
+computacional del problema, no s’espera que obtingui una solució 
+òptima. El que sí demanarem  és que sigui ràpid: ha de ser instantani
+fins i tot en les instàncies més grans que proporcionem."
