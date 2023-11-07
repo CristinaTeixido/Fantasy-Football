c0ac427391b175c3b767f33ba47fdd87e8b992ee
CristinaTeixido <133266907+CristinaTeixido@users.noreply.github.com>
2023-11-07 22:16:00 +0100

    Create mh.cc

---
+++ mh.cc
@@ -0,0 +1,4 @@
+" Ha d’implementar una metaheurística de les explicades a 
+classe (excepte Basic Local Search). Com amb la cerca exhaustiva, 
+volem que cada vegada que obtingui una solució millor, la 
+sobreescrigui a l’arxiu de sortida."
