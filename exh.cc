0dd935bcd676bb6640db4316ea1f5049e58858ed
CristinaTeixido <133266907+CristinaTeixido@users.noreply.github.com>
2023-11-07 22:14:05 +0100

    Create exh.cc

---
+++ exh.cc
@@ -0,0 +1,6 @@
+" Ha d’implementar una cerca exhaustiva. Cada vegada que obtingui una 
+  solució millor, l’ha de sobreescriure a l’arxiu de sortida. És a dir, 
+  volem que si avortem el programa, dins l’arxiu de sortida hi hagi la millor 
+  solució trobada fins al moment. No es pot combinar l’algorisme de cerca 
+  exhaustiva amb el greedy o la metaheurística. És a dir, ha de ser una 
+  cerca exhaustiva pura. "
