962a77ebc178f7563f1f4a5d67a23628ff837b54
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-11-22 14:25:26 +0100

    Primera idea exh.cc

--- exh.cc
+++ exh.cc
@@ -1,10 +1,3 @@
-" Ha d’implementar una cerca exhaustiva. Cada vegada que obtingui una 
-  solució millor, l’ha de sobreescriure a l’arxiu de sortida. És a dir, 
-  volem que si avortem el programa, dins l’arxiu de sortida hi hagi la millor 
-  solució trobada fins al moment. No es pot combinar l’algorisme de cerca 
-  exhaustiva amb el greedy o la metaheurística. És a dir, ha de ser una 
-  cerca exhaustiva pura. "
-
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -13,6 +6,7 @@
 #include <string>
 
 using namespace std;
+
 
 class Player {
     public:
@@ -27,13 +21,69 @@ class Player {
         id(ident), name(n), position(pos), price(pr), club(c), points(p){}
 };
 
+// Declarem dades globals de la consulta
+uint nDef, nMig, nDav, N;
+int maxTotalPrice, maxIndivPrice, nTotalPlayersDataset; 
+
 vector<Player> id2player;
+vector<Player> pors;
+vector<Player> defs;
+vector<Player> migs;
+vector<Player> davs;
 
-void tactica_exh(const string& sortida, uint nDef, uint nMig, 
-              uint nDav, int maxTotalPrice, int maxIndivPrice){
 
-    ofstream sortida(sortida);
-            
+vector<string> selected_players;
+
+
+
+void tactica_exh(const string& output, vector<bool>& used, vector<string>& selected_players, int sum_points, int sum_price, int sp, int last_selected){
+
+    if (sp == N) //escriu sortida al arxiu si sum_points > punts_max_moment;
+
+    else {
+
+    if (sp == 0)
+        for (uint p = 0; p < pors.size();++p){
+            // si el p + sum_price > maxTotalPrice  >> continue
+
+            if (not used[pors[p].id]){
+                used[pors[p].id] = true;
+                selected_players[0] = pors[p].name;
+                tactica_exh(argv[3], used, selected_players, sum_points+pors[p].points, sum_price+pors[p].price, 1, -1)
+                used[pors[p].id] = false;
+            }
+        }
+    
+    else if (sp > 0 && sp <= nDef){
+        for (uint de = last_selected+1; de < defs.size(); ++de){
+            used[defs[de].id] = true;
+            selected_players[sp] = defs[de].name;
+            tactica_exh(argv[3], used, selected_players, sum_points+defs[de].points, sum_price+defs[de].price, sp+1, de)
+            used[defs[de].id] = false;
+        }
+
+    else if (sp > nDef && sp <= nMig){
+        for (uint m = 0; m < migs.size(); ++m){
+            used[migs[m].id] = true;
+            selected_players[sp] = migs[m].name;
+            tactica_exh(argv[3], used, selected_players, sum_points+migs[m].points, sum_price+migs[m].price, sp+1)
+            used[migs[m].id] = false;
+        }
+    }
+    
+    else {
+        for (uint da = 0; da < davs.size(); ++da){
+            used[davs[da].id] = true;
+            selected_players[sp] = davs[da].name;
+            tactica_exh(argv[3], used, selected_players, sum_points+davs[da].points, sum_price+davs[da].price, sp+1)
+            used[davs[da].id] = false;
+            }
+        }
+    }
+
+    }
+
+    ofstream sortida(output);   
 }
 
 int main(int argc, char* argv[]) {
@@ -59,16 +109,37 @@ int main(int argc, char* argv[]) {
         getline(jugadors,aux2);
         id2player.push_back(Player(nextId++, name, position, price, club, p));
     }
+
+    nTotalPlayersDataset = nextId;
     jugadors.close();
 
     // Llegeix fitxer_consulta
     ifstream consulta(argv[2]);
-    uint nDef, nMig, nDav;
-    int maxTotalPrice, maxIndivPrice; 
     consulta >> nDef >> nMig >> nDav >> maxTotalPrice >> maxIndivPrice;
     consulta.close();
 
+    N = 1+nDef+nMig+nDav;
+
+    // Classifiquem tots els jugadors segona la seva posició
+    for (Player& p: id2player){
+        if (p.price > maxIndivPrice) continue;
+        if (p.position == "por") pors.push_back(p);
+        else if (p.position == "def") defs.push_back(p);
+        else if (p.position == "mig") migs.push_back(p);
+        else davs.push_back(p)
+    }
+
     // Crida la tactica de cerca exhaustiva
-    tactica_exh(argv[3], nDef, nMig, nDav, maxTotalPrice, maxIndivPrice);
+    vector<bool> used = vector<bool>(N + 1, false);
+    selected_players = vector<string>(N);
+
+    tactica_exh(argv[3], used, selected_players, 0, 0, 0, 0);
+
+    cout << "POR:" << endl;
+    cout << "DEF:" << endl;
+
+    cout << "MIG: " << endl;
+    cout << "Punts:" << endl;
+    cout << "Preu:" << endl;
 
 }
