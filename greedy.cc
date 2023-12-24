f0719811e5b840ab9252b35d48427fadf8e0db02
CristinaTeixido <133266907+CristinaTeixido@users.noreply.github.com>
2023-12-25 00:45:32 +0100

    Update greedy.cc comentat

--- greedy.cc
+++ greedy.cc
@@ -5,7 +5,7 @@
 #include <cassert>
 #include <string>
 #include <iomanip>
-#include <numeric> // Added to include accumulate
+#include <numeric>
 #include <cmath>
 
 using namespace std;
@@ -30,7 +30,7 @@ class Player {
         return (name != b.name);
     }
 
-    int position_num(){
+    int position_num(){  // Retorna 0, 1 o 2 segons la posició del jugador
         if (position == "por") return 0;    
         if (position == "def") return 1;
         if (position == "mig") return 2;
@@ -52,23 +52,27 @@ class Team {
     Team(int n, vector<vector<Player>> m, int po, int pr):
         num_members(n), members(m), points(po), price(pr){};
     
-    bool operator==(const Team& b){
-        if (num_members != b.num_members || points != b.points || price != b.price || members[0][0].name != b.members[0][0].name) return false;
-        for (int i = 1; i<4; ++i){
-            for (uint j=0; j<members[i].size(); ++j){
+    bool operator==(const Team& b){ // Comparació de dos equips
+        if (num_members != b.num_members || points != b.points || price != b.price 
+            || members[0][0].name != b.members[0][0].name) return false;
+
+        for (int i = 1; i < 4; ++i){
+            for (uint j = 0; j < members[i].size(); ++j){
                 if (members[i][j].name != b.members[i][j].name) return false;
             }
         }
         return true;
     }
 
-    void add_member(Player p, int g){
+    // Afageix un jugador a l'equip augmentant els punts, el preu i el nombre de membres
+    void add_member(Player p, int g){ 
         members[g].push_back(p);
         price += p.price;
         points += p.points;
         num_members += 1;
     }
 
+    // Escriu la solució en el format correcte
     void write(int t_start, string output){
         ofstream fout(output);   
         int t_end = clock();
@@ -89,15 +93,15 @@ class Team {
 
 };
 
-// Declarem dades globals de la consulta
+// Declarem variables globals
 int nDef, nMig, nDav;
 int maxTotalPrice, maxIndivPrice; 
 int actual_max_points, t_start;
 string output;
-
 vector<Player> players;
 vector<int> num_pl_position;
 
+// Retrona true si pl es pot afegir a l'equip i les condicions de l'input es segueixen complint 
 bool accepted_player(Team& team, Player& pl, int g){
     if (int(team.members[g].size()) == num_pl_position[g]) return false;
     if (pl.price > maxIndivPrice) return false;
@@ -108,7 +112,7 @@ bool accepted_player(Team& team, Player& pl, int g){
 void write_solution(Team& selected_team){
     ofstream fout(output);   
 
-    // Comprovem que el fitxer s'obre correctament
+    // Comprova que el fitxer s'obre correctament
     if (!fout.is_open()) {
         cout << "No s'ha pogut obrir el fitxer de sortida correctament." << endl;
         exit(1); 
@@ -120,7 +124,8 @@ void write_solution(Team& selected_team){
 
 }
 
-
+/* Algorisme greedy: si pot afegir el jugador l'afegeix, sinó intenta 
+   amb el següent jugador (sempre complint les condicions de l'input) */
 void tactica_greedy(Team& selected_team){
     int pl = 0;
     while(pl < int(players.size()) && selected_team.num_members < 11){
@@ -138,9 +143,6 @@ void tactica_greedy(Team& selected_team){
     cout << "greedy terminated" << endl;
 }
 
-void add_player_list(vector<vector<Player>>& lists, Player p){
-    lists[p.position_num()].push_back(p);
-}
 
 void read(int argc, char** argv){
     // Llegeix les dades dels jugadors
@@ -157,12 +159,12 @@ void read(int argc, char** argv){
         string aux2;
         getline(jugadors,aux2);
 
+        // Creem la variable efficiency (ratio de punts/log(preu)) per cada jugador
         float efficiency;
         if (price != 0) efficiency = p/log(price);
         else if (p != 0) efficiency = 20;
         else efficiency = 0;
 
-        //Classifiquem els jugadors segons posició i separem els fakes
         players.push_back(Player(name, position, price, club, p, efficiency));
     }
 
@@ -183,15 +185,15 @@ int main(int argc, char** argv) {
 
     read(argc, argv);
 
-    // ordena els jugadors en ordre descendent respecte els punts/preu
+    // Ordena els jugadors en ordre descendent respecte el valor d'efficiency
     sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
         return a.efficiency > b.efficiency; 
     });    
 
-    // Inicialitzem el cronòmetre
+    // Inicialitza el cronòmetre
     t_start = clock();
 
-    // Inicialitzem l'equip
+    // Inicialitza l'equip
     Team selected_team = Team();
     num_pl_position = {1, nDef, nMig, nDav};
     output = argv[3];
